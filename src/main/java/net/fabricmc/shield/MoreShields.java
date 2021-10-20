9fd2f20c25763333c31214fee1c00d24c388cb91
JWGardiner <86971433+JWGardiner@users.noreply.github.com>
2021-10-20 18:29:07 +0100

    Added textures, lang files, models, fixed bugs and added specialised netherite support

--- src/main/java/net/fabricmc/shield/MoreShields.java
+++ src/main/java/net/fabricmc/shield/MoreShields.java
@@ -16,12 +16,9 @@ import net.minecraft.util.registry.Registry;
 
 public class MoreShields implements ModInitializer {
 
-	public static final ItemGroup MORE_SHIELDS = FabricItemGroupBuilder.build(
-			new Identifier("moreshields", "shields"),
-			() -> new ItemStack(Items.SHIELD));
-	public static final ItemGroup MORE_SHIELDS_ADDITIONS = FabricItemGroupBuilder.create(
-					new Identifier("moreshields", "shields_additions"))
-			.icon(() -> new ItemStack(Items.WOODEN_AXE))
+	public static final ItemGroup MORE_SHIELDS = FabricItemGroupBuilder.create(
+					new Identifier("moreshields", "shields"))
+			.icon(() -> new ItemStack(Items.SHIELD))
 			.build();
 
 	public static final Item WOOD_SHIELD = new FabricShieldItem(new FabricItemSettings().maxDamage(250).group(MORE_SHIELDS), 10, 13, Items.OAK_PLANKS);
@@ -30,16 +27,24 @@ public class MoreShields implements ModInitializer {
 	public static final Item DIAMOND_SHIELD = new FabricShieldItem(new FabricItemSettings().maxDamage(2000).group(MORE_SHIELDS), 10, 13, Items.DIAMOND);
 	public static final Item NETHERITE_SHIELD = new FabricShieldItem(new FabricItemSettings().maxDamage(2500).group(MORE_SHIELDS), 10, 13, Items.NETHERITE_INGOT);
 
-	public static final Item OVERWORLD_NETHERITE_SHIELD = new FabricShieldItem(new FabricItemSettings().maxDamage(3500).group(MORE_SHIELDS_ADDITIONS), 10, 13, Items.NETHERITE_INGOT);
-	public static final Item NETHER_NETHERITE_SHIELD = new FabricShieldItem(new FabricItemSettings().maxDamage(3500).group(MORE_SHIELDS_ADDITIONS), 10, 13, Items.NETHERITE_INGOT);
-	public static final Item END_NETHERITE_SHIELD = new FabricShieldItem(new FabricItemSettings().maxDamage(3500).group(MORE_SHIELDS_ADDITIONS), 10, 13, Items.NETHERITE_INGOT);
+
+
 	@Override
 	public void onInitialize() {
 
-		if (FabricLoader.getInstance().isModLoaded("specialisednetherite") == true) {
+		if (FabricLoader.getInstance().isModLoaded("specialnetherite") == true) {
+			 final ItemGroup MORE_SHIELDS_SPECIALNETHERITE = FabricItemGroupBuilder.create(
+							new Identifier("moreshieldsadditions", "specialised_netherite_shields"))
+					.icon(() -> new ItemStack(Items.SHIELD))
+					.build();
+			 final Item OVERWORLD_NETHERITE_SHIELD = new FabricShieldItem(new FabricItemSettings().maxDamage(3500).group(MORE_SHIELDS_SPECIALNETHERITE), 10, 13, Items.NETHERITE_INGOT);
+			 final Item NETHER_NETHERITE_SHIELD = new FabricShieldItem(new FabricItemSettings().maxDamage(3500).group(MORE_SHIELDS_SPECIALNETHERITE), 10, 13, Items.NETHERITE_INGOT);
+			 final Item END_NETHERITE_SHIELD = new FabricShieldItem(new FabricItemSettings().maxDamage(3500).group(MORE_SHIELDS_SPECIALNETHERITE), 10, 13, Items.NETHERITE_INGOT);
 			Registry.register(Registry.ITEM, new Identifier("moreshieldsadditions", "overworld_netherite_shield"), OVERWORLD_NETHERITE_SHIELD);
 			Registry.register(Registry.ITEM, new Identifier("moreshieldsadditions", "nether_netherite_shield"), NETHER_NETHERITE_SHIELD);
 			Registry.register(Registry.ITEM, new Identifier("moreshieldsadditions", "end_netherite_shield"), END_NETHERITE_SHIELD);
+
+
 		}
 		//if (FabricLoader.getInstance().isModLoaded("specialisednetheriteplus") == true) {
 		//
